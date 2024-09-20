368a955aaf3480c44a848755ea624489555e5130
SShipon <nxshipon@gmail.com>
2024-09-20 01:00:11 -0700

    solved

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quos, temporibus nemo aspernatur doloremque eaque sit facere tenetur hic labore?</h2>
+        </div>
+    );
+};
+
+export default Home;

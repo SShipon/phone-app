c2d058d16db00988db4737cb042a773900ec72bc
SShipon <nxshipon@gmail.com>
2024-09-20 00:26:42 -0700

    test brnach add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+
+
+const App = () => {
+  return (
+    <div>
+       <Home />
+       <About />
+       <Contact />
+    </div>
+  );
+};
+
+export default App;

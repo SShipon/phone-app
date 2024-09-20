6884b4057ca034858fa0fae85a12a6e280509f89
SShipon <nxshipon@gmail.com>
2024-09-20 02:00:24 -0700

    solved this error footer page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Navbar";
+import Footer from "../pages/Footer";
 
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div>
              <Navbar />
              <Outlet />
+             <Footer />
         </div>
     );
 };

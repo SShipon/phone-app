9d9192f72a2ead7770366e8f55dc5fb74176f0ea
SShipon <nxshipon@gmail.com>
2024-09-21 13:30:14 -0700

    p tag add

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@
 const About = () => {
   return (
     <div>
-      About me
+      About me  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam in nemo architecto eveniet aperiam, dolorem perferendis, hic ducimus voluptatibus sequi qui doloremque pariatur nam harum blanditiis, commodi praesentium tempora ex aliquam. Quam, quos. Modi quo non, eveniet dolorum eius possimus quisquam rem sapiente laudantium alias sed odit ratione fuga soluta.
     </div>
   );
 }

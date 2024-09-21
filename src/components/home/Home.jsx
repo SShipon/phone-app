995bfbd8d9773e49c90d5ad98dc550d71b01a037
Risal Shahed <risalshahed@gmail.com>
2024-09-21 13:23:20 +0600

    home edited

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,9 @@ const Home = () => {
     return (
         <div>
             <h1>Risal Shahed&apos;s editing</h1>
+            <p>
+                A quick brown fox jumps over the lazy dog
+            </p>
         </div>
     );
 };
